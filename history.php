dd36d81ea295803ebc4509ce26cceeb9dc101d3b
Nikita Rimitsen <nikita.rimitsen@outlook.com>
2023-04-02 23:57:29 +0300

    Page, footer added, there is a navigation menu

---
+++ history.php
@@ -0,0 +1,22 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Ajalugu Tapa Katleri</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<ul>
+    <h1 id="tapa">Tapa Katleri</h1>
+    <h1 id="taekwondo">Taekwondo</h1>
+    <li><a href="main.php">Peamine</a></li>
+    <li><a href="information.php">Teave</a></li>
+    <li><a href="history.php">Ajalugu</a></li>
+    <li><a href="yearplan.php">Aastane plaan</a></li>
+</ul>
+</body>
+
+</html>
---
+++ main.php
@@ -0,0 +1,56 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Spordiklubi Tapa Katleri</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<ul>
+    <h1 id="tapa">Tapa Katleri</h1>
+    <h1 id="taekwondo">Taekwondo</h1>
+<li><a href="main.php">Peamine</a></li>
+<li><a href="information.php">Teave</a></li>
+<li><a href="history.php">Ajalugu</a></li>
+<li><a href="yearplan.php">Aastane plaan</a></li>
+</ul>
+</body>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+<!--footer starts from here-->
+<footer class="footer">
+    <div class="container bottom_border">
+        <div class="row">
+            <div class=" col-sm-4 col-md col-sm-4  col-12 col">
+                <h4 class="headin5_amrc col_white_amrc pt2">Spordiklubi Tapa Katleri</h4>
+                <!--headin5_amrc-->
+                <p class="mb10">Tervislik, kvaliteetne ja motiveeritud eluviis läbi spordi!</p>
+                <p><i class="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35 </p>
+                <p><i class="fa fa-phone"></i>  +91-9999878398  </p>
+                <p><i class="fa fa fa-envelope"></i> info@example.com  </p>
+            </div>
+            <div class=" col-sm-4 col-md  col-6 col">
+                <h4 class="headin5_amrc col_white_amrc pt2">Tapa</h4>
+                <div class="textwidget"><div class="icon-block">
+                        <p><i class="fa fa-envelope"></i>&nbsp; tapa.katleri@gmail.com</p>
+                    </div>
+                    <div class="icon-block">
+                        <p><i class="fa fa-mobile"></i>&nbsp; +372 5624 6990</p>
+                    </div>
+                    <div class="icon-block">
+                        <p><i class="fa fa-building"></i>&nbsp; Tapa Vene Gümnaasium, Nooruse 2</p>
+                    </div>
+                    <div class="icon-block">
+                        <p><i class="fa fa-briefcase"></i>&nbsp; Spordiklubi Tapa Katleri</p>
+                    </div>
+            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</html>
